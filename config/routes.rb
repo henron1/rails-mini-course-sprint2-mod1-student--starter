6947fac2f60b95c51b8ddc6fe59f0e521e6748fe
Henry Neal <henry.neal0@gmail.com>
2019-08-19 11:02:30 -0400

    step 1.5 done

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :customers
+      resources :customers do 
+        resources :orders, only:[:index, :create]
+      end
       resources :products, only: [:index, :show]
+      post "orders/:id/ship", to: "orders#ship"
+      resources :orders, only: [:index, :show] do
+        resources :products, [:index, :create]
+      end
+      
     end
   end
 end
